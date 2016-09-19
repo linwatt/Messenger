7b19e4b512ead7a4954e914bfa992578a1d89cf3
linwatt <lwatters@riis.com>
2016-09-19 12:14:40 -0400

    Initial commit, App up to pages 84-85: test drive that ReceiveMessageActivity is launched when the send button is pressed

---
+++ app/src/main/java/com/hfad/messenger/CreateMessageActivity.java
@@ -0,0 +1,21 @@
+package com.hfad.messenger;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    //Call onSendMessage() when the button is clicked
+    public void onSendMessage(View view){
+        Intent intent = new Intent(this, ReceiveMessageActivity.class);
+        startActivity(intent);
+    }
+}
